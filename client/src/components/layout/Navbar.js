e99122356449c028134dfb55bc7640a5d906414a
manovarun <samvarun@gmail.comn>
2019-11-23 15:17:26 +0530

    Cleanup, Navbar & Router Setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt"
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+export default Navbar;

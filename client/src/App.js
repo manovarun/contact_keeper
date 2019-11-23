7d1152c737f220b14d6c67d5087b6a6d19138331
manovarun <samvarun@gmail.comn>
2019-11-23 15:58:51 +0530

    Contacts Context & Global State

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import ContactState from "./components/context/contact/ContactState";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 

7aa3c34cbd317eb904b937d078fe3da3e328cf58
manovarun <samvarun@gmail.comn>
2019-11-23 23:13:57 +0530

    Auth Context & Initial State

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./components/context/contact/ContactState";
+import AuthState from "./components/context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
